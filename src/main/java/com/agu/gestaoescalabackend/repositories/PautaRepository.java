ab45c818991a18e747efcc53f896b3e52c5a22d4
mlobato447 <83079439+mlobato447@users.noreply.github.com>
2022-03-10 00:02:22 -0300

    perf: Melhoramento de performance.

--- src/main/java/com/agu/gestaoescalabackend/repositories/PautaRepository.java
+++ src/main/java/com/agu/gestaoescalabackend/repositories/PautaRepository.java
@@ -27,11 +27,11 @@ public interface PautaRepository extends JpaRepository<Pauta, Long> {
 
 	List<Pauta> findAllByOrderByIdAsc();
 
-	@Query("SELECT pauta FROM Pauta pauta WHERE (:hora is null or pauta.hora = :hora) AND (:vara is null or pauta.vara = :vara) AND (:sala is null or pauta.sala = :sala) AND (:pautista is null or pauta.pautista = :pautista) AND pauta.pautista is not null AND (pauta.data BETWEEN :dataInicial AND :dataFinal) Order by pauta.id ASC")
+	@Query("SELECT pauta FROM Pauta pauta WHERE (:hora is null or pauta.hora = :hora) AND (:vara is null or pauta.vara = :vara) AND (:sala is null or pauta.sala = :sala) AND (:pautista is null or pauta.pautista = :pautista) AND pauta.pautista is not null AND (pauta.data BETWEEN :dataInicial AND :dataFinal) Order by pauta.data ASC")
 	Page<Pauta> findAllByHoraAndVaraAndSalaAndPautistaAndDataBetween(String hora, String vara, String sala,
 			Pautista pautista, LocalDate dataInicial, LocalDate dataFinal, Pageable pageable);
 
-	@Query("SELECT pauta FROM Pauta pauta WHERE (:hora is null or pauta.hora = :hora) AND (:vara is null or pauta.vara = :vara) AND (:sala is null or pauta.sala = :sala) AND (:pautista is null or pauta.pautista = :pautista) AND pauta.pautista is not null ORDER BY pauta.id ASC")
+	@Query("SELECT pauta FROM Pauta pauta WHERE (:hora is null or pauta.hora = :hora) AND (:vara is null or pauta.vara = :vara) AND (:sala is null or pauta.sala = :sala) AND (:pautista is null or pauta.pautista = :pautista) AND pauta.pautista is not null ORDER BY pauta.data ASC")
 	Page<Pauta> findAllByHoraAndVaraAndSalaAndPautista(String hora, String vara, String sala, Pautista pautista,
 			Pageable pageable);
 
