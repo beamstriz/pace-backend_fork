593e20e61943225337402ba41741410aa42a9512
nutec-pfpa <nutecpfpa@gmail.com>
2023-05-24 21:30:44 -0300

    changed profile

--- src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
+++ src/main/java/com/agu/gestaoescalabackend/GestaoEscalaBackendApplication.java
@@ -5,14 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestaoEscalaBackendApplication implements CommandLineRunner {
+public class GestaoEscalaBackendApplication {
 
 	public static void main(String... args) {
 		SpringApplication.run(GestaoEscalaBackendApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("la la la");
-	}
 }
